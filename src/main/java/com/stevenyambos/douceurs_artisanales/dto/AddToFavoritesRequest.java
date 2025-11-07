18c35ff347ec3b597ccca9f1636a9f6d8f50a179
StevenYAMBOS <stevenyambos@gmail.com>
2025-11-07 11:03:20 +0100

    ref : Refactor en NestJS

--- src/main/java/com/stevenyambos/douceurs_artisanales/dto/AddToFavoritesRequest.java
+++
@@ -1,9 +0,0 @@
-package com.stevenyambos.douceurs_artisanales.dto;
-
-import lombok.Data;
-
-@Data
-public class AddToFavoritesRequest {
-    private String bakeryId;
-    private String userEmail;
-}
