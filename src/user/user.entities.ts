2e042d59b1c0dd20b198548843032361bdde518b
StevenYAMBOS <stevenyambos@gmail.com>
2025-12-19 12:28:57 +0100

    fix : Modèle d'un utilisateur

--- src/user/user.entities.ts
+++ src/user/user.entities.ts
@@ -5,15 +5,36 @@ export class User {
   @PrimaryGeneratedColumn('uuid')
   id: number;
 
-  @Column({ unique: false })
-  firstname: string;
-
-  @Column({ unique: false })
-  lastname: string;
-
-  @Column({ unique: true })
+  @Column({ unique: true, length: 100 })
   email: string;
 
-  @Column()
+  @Column({ unique: false, length: 50 })
+  first_name: string;
+
+  @Column({ unique: false, length: 50 })
+  last_name: string;
+
+  @Column({ length: 255 })
   password_hash: string;
+
+  @Column({ length: 15 })
+  phone: string;
+
+  @Column({ length: 20 })
+  role: string;
+
+  @Column()
+  is_verified: boolean;
+
+  @Column()
+  last_login: Date;
+
+  @Column()
+  is_active: boolean;
+
+  @Column()
+  created_at: Date;
+
+  @Column()
+  updated_at: Date;
 }
